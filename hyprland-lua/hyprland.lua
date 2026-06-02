f233d268244c630b0c9f5d149e97e3d2ef25c047
Ben Mann <14131644+BenMannMann@users.noreply.github.com>
2026-06-02 23:54:20 +0100

    Create hyprland.lua

---
+++ hyprland-lua/hyprland.lua
@@ -0,0 +1,19 @@
+-- This is an example Hyprland Lua config file.
+-- Refer to the wiki for more information.
+-- https://wiki.hypr.land/Configuring/Start/
+
+-- Please note not all available settings / options are set here.
+-- For a full list, see the wiki
+
+-- You can (and should!!) split this configuration into multiple files
+-- Create your files separately and then require them like this:
+require("lua.monitors")
+require("lua.programs")
+require("lua.autostart")
+require("lua.windows")
+require("lua.environment")
+require("lua.permissions")
+require("lua.looks")
+require("lua.input")
+require("lua.keybindings")
+require("lua.misc")
